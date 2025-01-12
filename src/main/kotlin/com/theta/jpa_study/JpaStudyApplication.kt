66f65f53c1bfcfc284e078b7f76fcf8b55c0e226
hnev <gpfm312@gmail.com>
2025-01-12 12:53:06 +0900

    [+] Create Project

---
+++ src/main/kotlin/com/theta/jpa_study/JpaStudyApplication.kt
@@ -0,0 +1,11 @@
+package com.theta.jpa_study
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JpaStudyApplication
+
+fun main(args: Array<String>) {
+	runApplication<JpaStudyApplication>(*args)
+}
